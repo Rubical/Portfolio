342ae873b2b539feab0967b5fda0f8db23e61e5d
Rubical <zhpiv96@mail.ru>
2023-02-25 15:09:55 +0300

    upd

--- src/components/helpers/projectsList.js
+++ src/components/helpers/projectsList.js
@@ -8,23 +8,23 @@ import project03Big from "./../../img/projects/03-big.jpg";
 const projects = [
   {
     title: "Api website",
-    skills: "ReactJS, MaterialUI",
+    skills: "ReactJS, MaterialUI, Supabase",
     img: project01,
     imgBig: project01Big,
     gitHubLink: "https://github.com/Rubical/github-API",
     link: "https://rubical.github.io/github-API/",
   },
   {
-    title: "Sushi shop",
+    title: "Sushi cart",
     img: project02,
     imgBig: project02Big,
-    skills: "React",
+    skills: "ReactJS",
     gitHubLink: "https://github.com/Rubical/react-shop",
     link: "https://rubical.github.io/react-shop/",
   },
   {
     title: "Coffeeshop",
-    skills: "Javascript, Bootstrap, SCSS",
+    skills: "VanillaJs, Bootstrap, SCSS",
     img: project03,
     imgBig: project03Big,
     gitHubLink: "https://github.com/Rubical/Coffeeshop",
